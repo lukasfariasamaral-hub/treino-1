3d0ef7ad0f78d67c43251cff24d022da3fc4e72a
Lukas <lukasfarias.amaral@gmail.com>
2026-08-02 18:09:11 +0100

    total de impostos por cliente

--- script.py
+++ script.py
@@ -2,4 +2,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 df = pd.read_csv('dataset_financeiro_treino.csv')
 
-print(df['status'].unique())
+# Filtrar só as linhas de imposto
+df_impostos = df[df['categoria'] == 'Impostos']
+
+# Somar o valor gasto em impostos, por cliente
+gastos_impostos = df_impostos.groupby('cliente')['valor'].sum().sort_values()
+print(gastos_impostos)
+
+gastos_impostos.plot(kind= 'bar', title= "total impostos por cliente")
+plt.xlabel('clientes')
+plt.ylabel('valor')
+plt.show()
